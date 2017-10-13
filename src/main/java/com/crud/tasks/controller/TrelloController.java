47f5b6524586b496349edac9827b1ae5926c46b8
mizaniewicz <mizaniewicz@gmail.com>
2017-10-13 11:47:27 +0200

    commit

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -13,6 +13,7 @@ import java.util.Objects;
 
 @RestController
 @RequestMapping("/v1/trello")
+@CrossOrigin(origins = "*")
 public class TrelloController {
     @Autowired
     private TrelloClient trelloClient;
