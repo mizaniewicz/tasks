6761c4ab699d9f3096139be94fe39404809dcf8e
mizaniewicz <mizaniewicz@gmail.com>
2017-10-14 14:55:56 +0200

    commit

--- src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -22,7 +22,7 @@ public class EmailScheduler {
     private AdminConfig adminConfig;
 
     //    @Scheduled(cron = "0 0 10 * * *")
-    @Scheduled(fixedDelay = "10000")
+    @Scheduled(fixedDelay = 10000)
     public void sendInformationEmail() {
         long size = taskRepository.count();
         simpleEmailService.send(new Mail(adminConfig.getAdminMail(), null, SUBJECT, "Currently in database you got " + size + " tasks"));
