e6ab799286a0f2a0afd167660b7d227549822103
mizaniewicz <mizaniewicz@gmail.com>
2017-11-19 16:56:07 +0100

    m24z4 - refaktor testów controller’a i deploy

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class MailCreatorService {
     @Autowired
@@ -17,6 +20,11 @@ public class MailCreatorService {
     private TemplateEngine templateEngine;
 
     public String buildTrelloCardEmail(String message) {
+        List<String > functionality = new ArrayList<>();
+        functionality.add("You can manage your tasks");
+        functionality.add("Provides connection with Trello Account");
+        functionality.add("Application allows sending tasks to Trello");
+
         Context context = new Context();
         context.setVariable("message", message);
         context.setVariable("tasks_url", "http://localhost:8080/crud");
@@ -25,6 +33,20 @@ public class MailCreatorService {
         context.setVariable("goodbye_message", "Best regards");
         context.setVariable("company_name", adminConfig.getCompanyName());
         context.setVariable("company_address", adminConfig.getCompanyAddress());
+        context.setVariable("show_button", false);
+        context.setVariable("is_friend", true);
+        context.setVariable("admin_config", adminConfig);
+        context.setVariable("application_functionality", functionality);
         return templateEngine.process("mail/created-trello-card-mail", context);
+    }
+
+    public String buildInformationEmail(String message) {
+        Context context = new Context();
+        context.setVariable("message", message);
+        context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("goodbye_message", "Best regards");
+        context.setVariable("company_name", adminConfig.getCompanyName());
+        context.setVariable("company_address", adminConfig.getCompanyAddress());
+        return templateEngine.process("mail/information-mail", context);
     }
 }
