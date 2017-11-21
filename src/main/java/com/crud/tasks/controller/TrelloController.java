ef0d169795a00c304d181a93d448134c0fd9e1d0
mizaniewicz <mizaniewicz@gmail.com>
2017-11-21 12:16:44 +0100

    m24z3 - mail

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -17,7 +17,7 @@ public class TrelloController {
     @Autowired
     private TrelloFacade trelloFacade;
 
-    @RequestMapping(method = RequestMethod.GET, value = "/boards")
+    @RequestMapping(method = RequestMethod.GET, value = "getTrelloBoards")
     public List<TrelloBoardDto> getTrelloBoards() {
         return trelloFacade.fetchTrelloBoards();
 
@@ -31,7 +31,7 @@ public class TrelloController {
 //                });
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "/cards")
+    @RequestMapping(method = RequestMethod.POST, value = "createTrelloCard")
     public CreatedTrelloCardDto createdTrelloCard(@RequestBody TrelloCardDto trelloCardDto) {
         return trelloFacade.createCard(trelloCardDto);
     }
